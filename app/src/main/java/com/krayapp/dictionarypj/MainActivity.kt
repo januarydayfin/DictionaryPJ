4cb742f51b147d00bd58f5a8bdfbf7139a8b8ada
krayman <50882897+krayman@users.noreply.github.com>
2021-10-30 13:22:26 +0700

    Меню возврата

--- app/src/main/java/com/krayapp/dictionarypj/MainActivity.kt
+++ app/src/main/java/com/krayapp/dictionarypj/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.krayapp.dictionarypj
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Router
 import com.github.terrakok.cicerone.androidx.AppNavigator
 import com.krayapp.dictionarypj.view.Screens.MainFragmentScreen
+import com.krayapp.dictionarypj.R.menu.*
 import com.krayapp.movieapppoplib.view.abs.AbsActivity
 import javax.inject.Inject
 
@@ -21,8 +24,22 @@ class MainActivity : AbsActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         savedInstanceState ?: router.newRootScreen(MainFragmentScreen)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(main_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.home_menu ->{
+                router.replaceScreen(MainFragmentScreen)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
     override fun onResumeFragments() {
         super.onResumeFragments()
         navigationHolder.setNavigator(navigator)
